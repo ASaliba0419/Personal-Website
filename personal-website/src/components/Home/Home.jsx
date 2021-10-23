099a2aa9796fa2c02a5b1e1a7ceb1b508d6ac4b6
ASaliba0419 <salibacodes@gmail.com>
2021-10-23 16:33:13 -0400

    updated responsiveness for language pics

--- personal-website/src/components/Home/Home.jsx
+++ personal-website/src/components/Home/Home.jsx
@@ -10,26 +10,27 @@ const Home = () => {
           <br /> -Benjamin Franklin
         </h2>
 
-          <img src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634584699/Portfolio/About%20Me/IMG_3831_glmb29.jpg" id='about-me-photo' alt="Andrew Saliba" />
-          <div className="about-info-container">
-        <p id='about-info'>
-        I am an audio/software engineer with a passion for technology, public speaking, and design. My extensive retail experience has allowed me to thrive in any team-oriented, fast-paced environment, as well as to adapt to various new challenges. No matter what team I am a part of, my creativity will always shine. 
+        <img src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634584699/Portfolio/About%20Me/IMG_3831_glmb29.jpg" id='about-me-photo' alt="Andrew Saliba" />
+        <div className="about-info-container">
+          <p id='about-info'>
+            I am an audio/software engineer with a passion for technology, public speaking, and design. My extensive retail experience has allowed me to thrive in any team-oriented, fast-paced environment, as well as to adapt to various new challenges. No matter what team I am a part of, my creativity will always shine.
           </p>
-          </div>
+        </div>
 
         <a href="https://drive.google.com/file/d/1qBWmniBBHZjcdNRaLjo48dKGT1yysYbS/view?usp=sharing" id='resume-home' target='_blank'><h2>Check out my Resumé here!</h2></a>
-        
+
         <div className="skillphotos">
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/download_itbdal.png" alt="HTML" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/cssLogo.4e67641b_voe6xv.png" alt="CSS" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/js-logo.838ce517_ozjzj6.png" alt="JS" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/react_js_logo.6e086d6c_sutsry.png" alt="REACT" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/node_js_logo.67ddd61a_u7lmpf.png" alt="NodeJs" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/postgreSqlLogo.53535cdd_u918uu.png" alt="SQL" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/mongodb-logo.0ceb38c8_uwjmkp.png" alt="MongoDB" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/gitLogo.dcc8b2cb_knnnoj.png" alt="git" />
-        <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/rubyOnRailsLogo.e20d131d_mq6mlv.png" alt="Ruby" />
-        
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/download_itbdal.png" alt="HTML" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/cssLogo.4e67641b_voe6xv.png" alt="CSS" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/js-logo.838ce517_ozjzj6.png" alt="JS" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/react_js_logo.6e086d6c_sutsry.png" alt="REACT" />
+        </div>
+        <div className="skillphotos2">
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/node_js_logo.67ddd61a_u7lmpf.png" alt="NodeJs" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/postgreSqlLogo.53535cdd_u918uu.png" alt="SQL" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653967/Portfolio/Skills/mongodb-logo.0ceb38c8_uwjmkp.png" alt="MongoDB" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/gitLogo.dcc8b2cb_knnnoj.png" alt="git" />
+          <img id='skill-photos' src="https://res.cloudinary.com/dtp9f7dv2/image/upload/v1634653968/Portfolio/Skills/rubyOnRailsLogo.e20d131d_mq6mlv.png" alt="Ruby" />
         </div>
       </div>
     </>
