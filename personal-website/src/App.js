2dc1038cc62ac1fb280aa0966b8c1b1165cdd669
ASaliba0419 <salibacodes@gmail.com>
2021-11-09 13:16:07 -0500

    Added dark mode

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -5,10 +5,27 @@ import Contact from './components/Contact/Contact.jsx'
 import Portfolio from './components/Portfolio/Portfolio'
 import Home from './components/Home/Home'
 import { Route } from 'react-router-dom'
+import Darkmode from 'darkmode-js'
 // TODO: tab
 // COMPLETED: tab
 
 const App = () => {
+
+  new Darkmode().showWidget()
+  const options = {
+    bottom: '64px', 
+    time: '0.5s',
+    mixColor: '#fff', 
+    backgroundColor: '#e9d9c3', 
+    buttonColorDark: '#100f2c', 
+    buttonColorLight: '#fff',
+    saveInCookies: false,
+    label: '🌓',
+    autoMatchOsTheme: true,
+  }
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
 
   return (
     <>
--- personal-website/src/components/Home/Home.jsx
+++ personal-website/src/components/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Home.css'
 
 const Home = () => {
+
+
   return (
     <>
       <div className='home-container'>
